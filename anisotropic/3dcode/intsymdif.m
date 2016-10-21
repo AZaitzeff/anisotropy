f7d1b24ccdb5a1c81abf46635529824333ed7377
Alexander Zaitzeff <azaitzeff@gmail.com>
2016-10-21 16:49:06 -0400

    try try again

--- anisotropic/3dcode/intsymdif.m
+++ anisotropic/3dcode/intsymdif.m
@@ -12,7 +12,8 @@ for k=1:N1
     total=sum(sum(data(k,1,:,:)));
     for i=1:N2;
         u=tdphysicalK3d(t(i),0.001,reshape(u0(k,:,:,:),[sizeu sizeu sizeu]),coeff1,coeff2);
-        symdif=sum(sum(sum((imgaussfilt3(reshape(data(k,i,:,:,:),[sizeu sizeu sizeu]),sigma)-imgaussfilt3(u,sigma)).^2)));
+        %symdif=sum(sum(sum((imgaussfilt3(reshape(data(k,i,:,:,:),[sizeu sizeu sizeu]),sigma)-imgaussfilt3(u,sigma)).^2)));
+        symdif=sum(sum(sum((i(reshape(data(k,i,:,:,:),[sizeu sizeu sizeu]),sigma)-(u,sigma)).^2)));
         val=val+symdif*sqrt(total/(sum(sum(sum(data(k,i,:,:,:))))+1));
     end
 end
