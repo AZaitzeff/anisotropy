ad1fe0694bc54beab5c64c3e7484a73787f1bdd9
Alexander Zaitzeff <azaitzeff@gmail.com>
2016-11-01 14:44:52 -0400

    try try again

--- anisotropic/3dcode/startscript.m
+++ anisotropic/3dcode/startscript.m
@@ -20,14 +20,18 @@ end
 %coeff10=coeff1r+[0,.5,.5,0,.5,.5]; reg
 %coeff20=coeff1r+[0,0,.5,.5,.5,.1];
 
-coeff10=[-3.5448,0,0,0,0,0]; %3
-coeff20=[-3.5448,0,0,0,0,0];
+%coeff10=[-3.5448,0,0,0,0,0]; %3
+%coeff20=[-3.5448,0,0,0,0,0];
+
+coeff10=coeff1r+[0,0,.1,0,.1,.1]; %4
+coeff20=coeff1r+[0,0,.1,.1,.1,0];
+
 %start = tdphysicalK3d(t,0.001,u0,coeff10,coeff20);
 tic
-[coeff1m,coeff2m,vals]=gradientdescK(data,t,300,.000005,u0,coeff10,coeff20);
+[coeff1m,coeff2m,vals]=gradientdescK(data,t,400,.000001,u0,coeff10,coeff20);
 toc
 %u = tdphysicalK3d(t,0.001,u0,coeff1w,coeff2w);
-save('coeffs3.mat','coeff1m','coeff2m','vals')
+save('coeffs4.mat','coeff1m','coeff2m','vals')
 %isosurface(reshape(data(1,1,:,:,:),[sizeu sizeu sizeu]),0.5); axis([1 64 1 64 1 64]); axis square
 
        
