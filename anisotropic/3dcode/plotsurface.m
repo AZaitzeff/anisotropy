40f2a363f112bdb5f7768ad110294dae560638b8
Alexander Zaitzeff <azaitzeff@gmail.com>
2016-11-16 15:37:32 -0500

    add more

--- anisotropic/3dcode/plotsurface.m
+++ anisotropic/3dcode/plotsurface.m
@@ -1,5 +1,5 @@
 
-N1=8;
+N1=10;
 N2=2;
 sizeu=64;
 u0=zeros(N1,64,64,64);
@@ -11,6 +11,8 @@ u0(5,:,:,:)= getellipsoid(64,10,10,30,0,0,0);
 u0(6,:,:,:)= getellipsoid(64,30,10,10,3*pi/4,3*pi/4,pi/4);
 u0(7,:,:,:)= getellipsoid(64,30,30,10,pi/4,0,0);
 u0(8,:,:,:)= getellipsoid(64,10,30,30,0,0,0);
+u0(9,:,:,:)= getellipsoid(64,20,30,10,pi/6,3*pi/8,0);
+u0(10,:,:,:)= getellipsoid(64,10,20,30,pi/3,pi/8,pi/6);
 coeff1r=[-3.5448,-1,-2,0,-2,-1];
 coeff2r=[-3.5448,0,-1,-1,-1,0];
 t=[10,20];
@@ -62,6 +64,6 @@ for i =1:n;
     end
 end
 delete(gcp('nocreate'))
-save('surface3.mat','Z1','Z2','Z3','Z4','Z5')
+save('surface4.mat','Z1','Z2','Z3','Z4','Z5')
 
 %surf(w1,w2,Z)
