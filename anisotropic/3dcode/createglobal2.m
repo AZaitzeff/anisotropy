93b750bcfaa14d7932a4daafdf52b9228c932a44
Alexander Zaitzeff <azaitzeff@gmail.com>
2016-12-09 10:56:44 -0500

    globe

--- anisotropic/3dcode/createglobal2.m
+++ anisotropic/3dcode/createglobal2.m
@@ -1,15 +1,28 @@
 parpool('local',8)
 
 initializefunc
+
+value=0;
+coeff10=[-3.5448,-1,-2,0,-2,-.5];
+while(value<.2)
+    per=randn(1,5);
+    coeff10(2:6)=[-1,-2,0,-2,-.5]+per/norm(per)*(rand()*2+.5);
+    value=minval(coeff10);
+end
+value2=0;
+coeff20=[-3.5448,.1,-.1,-.1,-.1,.1];
+while(value2<.5)
+    per=randn(1,5);
+    coeff20(2:6)=[.1,-.1,-.1,-.1,.1]+per/norm(per)*(rand()*.25);
+    value2=minval(coeff20);
+end
 xstart=zeros(1,10);
-coeff10=[-3.5448,0,0,0,0,0];
-coeff20=[-3.5448,0,0,0,0,0];
-coeff10(2:6)=xstart(1:5);
-coeff20(2:6)=xstart(6:10);
+xstart(1:5)=coeff10(2:6);
+xstart(6:10)=coeff20(2:6);
 problem = createOptimProblem('fmincon','x0',xstart,'objective',energy,'nonlcon',@(x)constraintsSH(x));
 gs = GlobalSearch('Display','iter');
 [optx,fmin,exitflag,output,manymins] = run(gs,problem);
-filename=strcat('trialglobal1.mat');
+filename=strcat('trialglobal2.mat');
 changeback
 anistrophy
 difani0=sum(sum(sum(abs((ST0<1)-(STr<1)))));
--- anisotropic/3dcode/createglobal3.m
+++ anisotropic/3dcode/createglobal3.m
@@ -1,15 +1,21 @@
 parpool('local',8)
 
 initializefunc
+value=0;
+coeff10=[-3.5448,-1,-2,0,-2,-.5];
+while(value<.2)
+    per=randn(1,5);
+    coeff10(2:6)=[-1,-2,0,-2,-.5]+per/norm(per)*(rand()*2+.5);
+    value=minval(coeff10);
+end
+coeff20=[-3.5448,.1,-.1,-.1,-.1,.1];
 xstart=zeros(1,10);
-coeff10=[-3.5448,0,0,0,0,0];
-coeff20=[-3.5448,0,0,0,0,0];
-coeff10(2:6)=xstart(1:5);
-coeff20(2:6)=xstart(6:10);
+xstart(1:5)=coeff10(2:6);
+xstart(6:10)=coeff20(2:6);
 problem = createOptimProblem('fmincon','x0',xstart,'objective',energy,'nonlcon',@(x)constraintsSH(x));
 gs = GlobalSearch('Display','iter');
 [optx,fmin,exitflag,output,manymins] = run(gs,problem);
-filename=strcat('trialglobal1.mat');
+filename=strcat('trialglobal3.mat');
 changeback
 anistrophy
 difani0=sum(sum(sum(abs((ST0<1)-(STr<1)))));
